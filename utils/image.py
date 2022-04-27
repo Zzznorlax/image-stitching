fix(blending): fix blending alpha calculation

utils/image.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 
 
+def downscale(img: np.ndarray, scale: float = 0.5) -> np.ndarray:
+    return cv2.resize(img, (int(img.shape[1] * scale), int(img.shape[0] * scale)))
+
+
 def cylindrical_x(x: int, focal_length: float) -> float:
     return focal_length * math.atan(x / focal_length)
 
@@ -59,12 +63,16 @@ def x_blending(img: np.ndarray, start: int, end: int = -1) -> np.ndarray:
     start, end = min(start, end), max(start, end)
 
     x = np.zeros((width))
-    alpha = 1 / abs(interval)
+    alpha = min(abs(interval), width)
+
     if interval < 0:
-        x[start:end] = np.arange(start=0, stop=1, step=alpha)
+        x[start:end] = np.arange(start=0, stop=alpha)
+        x /= alpha
         x[end:] = 1
+
     else:
-        x[start:end] = np.arange(start=1, stop=0, step=-alpha)
+        x[start:end] = np.arange(start=alpha, stop=0, step=-1)
+        x /= alpha
         x[:start] = 1
 
     img = img.astype(np.float64)
@@ -98,6 +106,8 @@ def cylindrical_projection(img, focal_length):
     else:
         _, thresh = cv2.threshold(cylinder_proj, 1, 255, cv2.THRESH_BINARY)
 
+    return cylinder_proj
+
     contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     x, y, w, h = cv2.boundingRect(contours[0][0])
 
@@ -106,12 +116,16 @@ def cylindrical_projection(img, focal_length):
 
 if __name__ == '__main__':
 
-    filename = 'samples/0.jpeg'
-    img = cv2.imread(filename)
+    # filename = 'samples/mn_2/01.jpg'
+    # img = cv2.imread(filename)
 
-    img = img[:, :, 0]
+    # # warped_img = to_cylindrical(img, 1000)
+    # blended_img = x_blending(img, 0)
 
-    warped_img = to_cylindrical(img, 1000)
+    # cv2.imshow("warped", blended_img)
+    # cv2.waitKey(0)
 
-    cv2.imshow("warped", warped_img)
-    cv2.waitKey(0)
+    # cv2.imwrite("x_blending.jpg", blended_img)
+
+    a = np.arange(1, 0, step=-(1 / 99))
+    print(a, a.shape)

utils/msop.py
@@ -202,7 +202,7 @@ def match(map_a: WaveletHashmap, map_b: WaveletHashmap):
     return mv_pairs
 
 
-def ransac(mv_pairs: List, k: int = 2000, n: int = 2, diff_thres: float = 5) -> Tuple[Tuple[float, float], int]:
+def ransac(mv_pairs: List, k: int = 200, n: int = 1, diff_thres: float = 5, loop: bool = False, inlier_thres: float = 0.2) -> Tuple[Tuple[float, float], int]:
 
     num_pairs = len(mv_pairs)
 
@@ -211,7 +211,15 @@ def ransac(mv_pairs: List, k: int = 2000, n: int = 2, diff_thres: float = 5) -> 
 
     max_inlier = 0
     best_mv = (0, 0)
-    for _ in range(k):
+
+    counter = 0
+    while True:
+        if loop:
+            if max_inlier >= num_pairs * 0.2:
+                break
+
+        elif counter >= k:
+            break
 
         samples = pairs[np.random.choice(num_pairs, n)]
 
@@ -232,11 +240,14 @@ def ransac(mv_pairs: List, k: int = 2000, n: int = 2, diff_thres: float = 5) -> 
             inlier_mv[:, 0] = pairs[:, 0] - pairs[:, 2]
             inlier_mv[:, 1] = pairs[:, 1] - pairs[:, 3]
 
-            outlier_indices = np.where((inlier_mv > diff_thres).all(axis=1))
-            inlier_mv[outlier_indices[0], :] = 0
+            # print(samples)
+            # print(inlier_mv[inlier_indices])
 
             best_mv = (float(np.average(inlier_mv[inlier_indices, 0])), float(np.average(inlier_mv[inlier_indices, 1])))
 
+        counter += 1
+
+    print("Best MV {} from {} inliers".format(best_mv, max_inlier))
     return best_mv, max_inlier
 
 
@@ -282,7 +293,7 @@ def blend_imgs(img_a: np.ndarray, img_b: np.ndarray, mv_mat: Tuple[int, int]):
     if abs(height_a - height_b) > abs_mv_y:
         result_height = max(height_a, height_b)
 
-    result = np.zeros((result_height + abs_mv_y, result_width, channel), dtype=np.float64)
+    result = np.zeros((result_height, result_width, channel), dtype=np.float64)
 
     img_a = img_utils.x_blending(img_a, width_a - mv_x, width_a)
     img_b = img_utils.x_blending(img_b, mv_x, 0)